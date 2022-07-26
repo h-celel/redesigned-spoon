be96399338b7f29ff4c385cc60e1b1ea7073ac33
henrik <henrik@celel.se>
2022-07-26 14:45:28 +0200

    hard coded grpc-web test button

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,45 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <button @click="increment">Count is: {{ count }}</button>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import { HelloRequest, Message } from '../../proto/examples/helloworld_pb'
+import { ServicePromiseClient } from '../../proto/examples/helloworld_grpc_web_pb'
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    async increment() {
+      this.count++
+
+      let client = new ServicePromiseClient('http://localhost:9000', null, null)
+      const req = new HelloRequest()
+      const message = new Message()
+      message.setValue('hellooo')
+      req.setMessage(message)
+      try {
+        const res = await client.hello(req, {})
+        console.log(res.getMessage().getValue())
+      } catch (err) {
+        console.error(err.message)
+        throw err
+      }
+
+    }
+  },
+  mounted() {
+    console.log(`The initial count is ${this.count}.`)
   }
 }
 </script>
